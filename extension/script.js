6ee47f6c1b5ffb939cb3f633046b527edfe526c7
sada <sada@sadair.local>
2012-04-08 23:29:41 +0900

    swich repository from google code.

---
+++ extension/script.js
@@ -0,0 +1,17 @@
+(function(){
+  var option = null;
+  chrome.extension.sendRequest(null, function( response ){
+    extensionOptions = response;
+    main(extensionOptions);
+  });
+})();
+
+function main(extensionOptions){
+  var zen_textarea = zen_cte(extensionOptions);
+  zen_textarea.setup({pretty_break: true});
+  
+  var shortcuts = extensionOptions["shortcuts"];
+  for(var i=0; i<shortcuts.length; i++){
+    zen_textarea.addShortcut(shortcuts[i].key, shortcuts[i].action);
+  }
+}
